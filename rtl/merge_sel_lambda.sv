23ea39abf9210b01e1ca927e913fb9b58cb825b0
mawadrun <mawadrun@gmail.com>
2026-06-02 12:49:17 +0700

    fix: merge_sel_lambda for ZC_MEDIUM

--- rtl/merge_sel_lambda.sv
+++ rtl/merge_sel_lambda.sv
@@ -23,10 +23,16 @@ module merge_select_lambda #(
           end
         end
         ZC_MEDIUM: begin
-          for (int j = 0; j < NUM_CS/2; j++) begin
-            data_out[2*d_cycle][(NUM_CS-j)*ZC_PER_CS-1 -: ZC_PER_CS] = data_in[NUM_CS-j-3];
-            data_out[2*d_cycle+1][(NUM_CS-j)*ZC_PER_CS-1 -: ZC_PER_CS] = data_in[NUM_CS-j-1];
-          end
+          // d_cycle = 1: {data_in[1],data_in[0]} -> data_out[0],
+          //              {data_in[3],data_in[2]} -> data_out[1].
+          // d_cycle = 2: {data_in[1],data_in[0]} -> data_out[2],
+          //              {data_in[3],data_in[2]} -> data_out[3].
+          // Within each 192-bit row, the higher-index data_in occupies the
+          // MSB half; the row itself sits at the top of the 384-bit slot.
+          data_out[2*(d_cycle-1)  ][NUM_CS*ZC_PER_CS-1     -: ZC_PER_CS] = data_in[1];
+          data_out[2*(d_cycle-1)  ][(NUM_CS-1)*ZC_PER_CS-1 -: ZC_PER_CS] = data_in[0];
+          data_out[2*(d_cycle-1)+1][NUM_CS*ZC_PER_CS-1     -: ZC_PER_CS] = data_in[3];
+          data_out[2*(d_cycle-1)+1][(NUM_CS-1)*ZC_PER_CS-1 -: ZC_PER_CS] = data_in[2];
         end
         ZC_LARGE: begin
           for (int j = 0; j < NUM_CS; j++) begin
